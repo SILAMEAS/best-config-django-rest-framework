65571104b1da70bc542420b71c01f9e6fb24bc4d
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-02-04 22:07:30 +0700

    view-generic-class

--- api/views.py
+++ api/views.py
@@ -3,66 +3,28 @@ from api.serializers import ProductSerializer,OrderItemSerializer,OrderSerialize
 from api.models import Product,OrderItem,Order
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,generics
 from django.shortcuts import get_object_or_404,get_list_or_404
 from django.http import JsonResponse
 
-@api_view(['GET'])
-def product_list(request):
-    products= Product.objects.all()
-    serializer=ProductSerializer(products,many=True)
-    return Response(
-        serializer.data,
-        status=status.HTTP_200_OK
-    )
-
-@api_view(['GET','PUT',"DELETE"])
-def product_detail(request,pk):
-    product= get_object_or_404(Product,pk=pk)
-    if(request.method == 'GET'):
-        serializer= ProductSerializer(product)
-        return Response(serializer.data)
-    elif (request.method == 'PUT'):
-        serializer= ProductSerializer(product,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
-    elif request.method =='DELETE':
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
-
-
-@api_view(['GET'])
-def order_list(request):
-    orders= Order.objects.all()
-    serializer=OrderSerializer(orders,many=True)
-    return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
-
-@api_view(['GET','PUT',"DELETE"])
-def order_detail(request,pk):
-    order= get_object_or_404(Order,pk=pk)
-    if(request.method == 'GET'):
-        serializer= OrderSerializer(order)
-        return Response(serializer.data)
-    elif (request.method == 'PUT'):
-        serializer= OrderSerializer(order,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
-    elif request.method =='DELETE':
-        order.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductListAPIView(generics.ListAPIView):
+    # .filter(stock__gt=0)  filter product that has stock > 0
+    # .exclude(stock__gt=0) filter product that has stock == 0
+    queryset = Product.objects.prefetch_related('order_items','orders').filter(stock__gt=0)
+    serializer_class = ProductSerializer
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.prefetch_related('order_items','orders').all()
+    serializer_class = ProductSerializer
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items','items__product').all()
+    serializer_class = OrderSerializer
+class OrderDetailAPIView(generics.RetrieveAPIView):
+    queryset = Order.objects.prefetch_related('items','items__product').all()
+    serializer_class = OrderSerializer
 
 @api_view(['GET'])
 def product_info(request):
-    products = Product.objects.all()
+    products = Product.objects.prefetch_related("order_items").all()
     serializer = ProductInfoSerializer({
         'products': products,
         'count': len(products),
